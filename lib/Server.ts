874d16a62a830f5a6c009e3fa5102a32fd08785a
Bryan Maynard <bsgbryan@gmail.com>
2026-05-16 13:46:43 -0500

    Created a super-simple test client
    Also deleted some comments
    
    Closes #7

--- lib/Server.ts
+++ lib/Server.ts
@@ -5,39 +5,27 @@ import params from "./params"
 const rz = await madul('+RogueZero', params, `${__dirname}/..`)
 
 Bun.serve({
+  hostname: 'localhost',
+  port: params.port,
   fetch(req, server) {
     const url = new URL(req.url);
-    if (url.pathname === "/chat") {
-      console.log(`upgrade!`);
-      return server.upgrade(req) ?
-        new Response("Welcome! 🎉")
-        :
-        new Response("WebSocket upgrade error", { status: 400 });
-    }
-
-    return new Response("Rebelions are built on hope!");
+    console.log(`upgrade ${url.pathname}!`);
+    return server.upgrade(req) ?
+      new Response(`Welcome to ${url.pathname}! 🎉`)
+      :
+      new Response("WebSocket upgrade error", { status: 400 });
   },
   websocket: {
     perMessageDeflate: true,
-    // TypeScript: specify the type of ws.data like this
-    // data: {} as { username: string },
     open(ws) {
-      // const msg = `${ws.data.username} has entered the chat`;
-      // ws.subscribe("the-group-chat");
-      // server.publish("the-group-chat", msg);
+      console.log('connection opened')
     },
     message(ws, message) {
-      // this is a group chat
-      // so the server re-broadcasts incoming message to everyone
-      // server.publish("the-group-chat", `${ws.data.username}: ${message}`);
-
-      // inspect current subscriptions
-      console.log(ws.subscriptions); // ["the-group-chat"]
+      console.log(`message received: ${message}`)
+      console.log('subscriptions', ws.subscriptions); // ["the-group-chat"]
     },
     close(ws) {
-      // const msg = `${ws.data.username} has left the chat`;
-      // ws.unsubscribe("the-group-chat");
-      // server.publish("the-group-chat", msg);
+      console.log('connection closed')
     },
   },
 });
