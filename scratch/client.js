df57383989bafa3a0ec695f28971c5137cdd6ac6
Bryan Maynard <bsgbryan@gmail.com>
2026-05-16 18:15:11 -0500

    More client work

--- scratch/client.js
+++ scratch/client.js
@@ -1,52 +1,38 @@
 let counter = Number(localStorage.getItem('counter') ?? 0)
-let ws
 let ping
 let active = Boolean(localStorage.getItem('active'))
 
-const fn = () => {
+const fn = (ws) => () => {
   if (active) {
     console.log(`SENT: ping: ${counter}`)
     ws.send(`ping: ${counter++}`)
   }
 }
 
-const open = () => {
+const open = (ws) => () => {
   console.log("opened connection to", document.getElementById("url").value)
   active = true
 
   if (!ping) {
     console.log('initializing ping interval')
-    ping = setInterval(fn, 1000)
+    ping = setInterval(fn(ws), 1000)
   }
 }
 
-const close = () => {
+const clear = () => {
   console.log("server closed connection, clearing ping interval")
   clearInterval(ping)
   ping = undefined
-  ws = undefined
 }
 
-const reset = () => {
-  console.log('closing existing connection')
-  ws.close()
-  ws = undefined
+const init = (event) => {
+  event?.preventDefault()
 
-  if (ping) {
-    console.log('clearing existing ping interval')
-    window.clearInterval(ping)
-    ping = undefined
-  }
-}
+  if (ping) clear()
 
-const init = () => {
-  if (ws) reset()
-
-  const url = document.getElementById("url").value
-  ws = new WebSocket(url)
-
-  ws.addEventListener("open", open)
-  ws.addEventListener("close", close)
+  ws = new WebSocket(document.getElementById("url").value)
+  ws.addEventListener("open", open(ws))
+  ws.addEventListener("close", clear)
 }
 
 const hide = () => {
@@ -71,5 +57,7 @@ document.getElementById("url").value = localStorage.getItem('url')
 document.getElementById("connect").addEventListener("click", init)
 
 window.addEventListener("pagehide", hide)
+
+let ws = new WebSocket(document.getElementById("url").value)
 
 if (active) init()
